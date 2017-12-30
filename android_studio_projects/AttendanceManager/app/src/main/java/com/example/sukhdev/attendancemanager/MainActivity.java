c7063eaf3d616e0f5bfbe264bb27341b6902ded7
Sukhdev841 <singhsukhdev1415@gmail.com>
2017-12-30 15:36:37 +0530

    starting

---
+++ android_studio_projects/AttendanceManager/app/src/main/java/com/example/sukhdev/attendancemanager/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sukhdev.attendancemanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
